12506d8fa4b8ded07b7fdfefd9da305eaccda29a
Роман Красовский <krasovskiyroman@gmail.com>
2024-01-06 21:13:04 +0300

    logs added

--- kittybot.py
+++ kittybot.py
@@ -1,12 +1,38 @@
-import requests
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+"""Импорт для лога."""
+import logging
+"""Импорт для передачи токена Телеграм из оружения."""
+import os
 
-updater = Updater(token='6979293587:AAGaLUmvnnK6bdlrlGf1NGFglq9RFj9Hj80')
+import requests
+
+"""Импорт для визуализации кнопок диалога в Телеграм."""
+from telegram import ReplyKeyboardMarkup
+"""Импорт для обработки событий бота."""
+from telegram.ext import CommandHandler, Updater
+
+"""Импорт для лога."""
+from dotenv import load_dotenv
+
+"""Получение токена бота."""
+load_dotenv()
+
+secret_token = os.getenv('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
 URL = 'https://api.thecatapi.com/v1/images/search'
 
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
@@ -14,22 +40,30 @@ def new_cat(update, context):
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_image())
 
+
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
-    # За счёт параметра resize_keyboard=True сделаем кнопки поменьше
     button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
 
     context.bot.send_message(
         chat_id=chat.id,
-        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
         reply_markup=button
     )
 
     context.bot.send_photo(chat.id, get_new_image())
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
 
-updater.start_polling()
-updater.idle() 
+def main():
+    updater = Updater(token=secret_token)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
