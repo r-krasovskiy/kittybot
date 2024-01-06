0942060bef609d228941f2c7c960197a7f72aadf
Роман Красовский <krasovskiyroman@gmail.com>
2024-01-06 13:11:59 +0300

    request to pic storage added

--- kittybot.py
+++ kittybot.py
@@ -1,8 +1,35 @@
-"""Библиотека для работы с ботами Телеграм."""
-from telegram import Bot
+import requests
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-bot = Bot(token='6979293587:AAGaLUmvnnK6bdlrlGf1NGFglq9RFj9Hj80')
+updater = Updater(token='6979293587:AAGaLUmvnnK6bdlrlGf1NGFglq9RFj9Hj80')
+URL = 'https://api.thecatapi.com/v1/images/search'
 
-chat_id = 193802815
-text = 'Вам телеграмма!'
-bot.send_message(chat_id, text)
+def get_new_image():
+    response = requests.get(URL).json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+def new_cat(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image())
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    # За счёт параметра resize_keyboard=True сделаем кнопки поменьше
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        reply_markup=button
+    )
+
+    context.bot.send_photo(chat.id, get_new_image())
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+updater.start_polling()
+updater.idle() 
